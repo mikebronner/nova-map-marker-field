8f494409853a0e406a59fb5b5e8a35833aaae369
Taner Kucukyuruk <hello@taner.work>
2022-05-23 09:05:53 +0200

    simplify nova booting call

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,4 +1,4 @@
-Nova.booting((Vue, router, store) => {
+Nova.booting((Vue) => {
     Vue.component('index-nova-map-marker-field', require('./components/IndexField.vue').default);
     Vue.component('detail-nova-map-marker-field', require('./components/DetailField.vue').default);
     Vue.component('form-nova-map-marker-field', require('./components/FormField.vue').default);
